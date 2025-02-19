59be23ce13bd43a61391c762b0d54c8138ac8fcb
semakenjhb024 <semakenjhb024@student.wethinkcode.co.za>
2025-02-19 15:27:02 +0200

    my letter program

---
+++ Letter.java
@@ -0,0 +1,17 @@
+public class Letter {
+    public static void main(String[] arg){
+        System.out.println("+-------------------------------------------------------+");
+        System.out.println("|                                                  #### |");
+        System.out.println("|                                                  #### |");
+        System.out.println("|                                                  #### |");
+        System.out.println("|                                                       |");
+        System.out.println("|                                                       |");
+        System.out.println("|                                 Sello Makena          |");
+        System.out.println("|                                 132 WeThinkCode       |");
+        System.out.println("|                                 Jan Smats Avenue      |");
+        System.out.println("|                                                       |");
+        System.out.println("+-------------------------------------------------------+");
+
+
+    }    
+}
